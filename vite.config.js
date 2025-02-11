24d47a5bfab55f392358fdd78c6091c107abd1c5
shashwatsinghkharola <sshaswat196@gmail.com>
2025-02-11 15:20:24 +0530

    Second changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {port:300 , host:true}
+  server : {port:3000, host:true}
 })
