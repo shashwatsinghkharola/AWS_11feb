fb822e7bd1a14e608d13af88572beb26c6d1b37a
shashwatsinghkharola <sshaswat196@gmail.com>
2025-02-11 14:52:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Welcome to EC2 instance</h1>
+        <h2>Launch......!</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
